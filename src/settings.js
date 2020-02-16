ea52a9f6c7021f669fcb92e5d826796664418e91
tperale <thomas.perale@openmailbox.org>
2020-02-16 15:39:07 +0100

    [enh] Put more information in the env file

---
+++ src/settings.js
@@ -0,0 +1,24 @@
+
+export default {
+  title: process.env.REACT_APP_WEBSITE_TITLE,
+  short_title: process.env.REACT_APP_WEBSITE_SHORT_TITLE,
+  subtitle: "",
+  description: process.env.REACT_APP_WEBSITE_DESC,
+  url: process.env.REACT_APP_WEBSITE_URL,
+  location: {
+    entreprise: {
+      name: "Le Boiler",
+      url: "https://www.facebook.com/1Boiler/"
+    },
+    number: "299",
+    street: "Rue Léopold 1er",
+    CP: "1020",
+    town: "Laeken",
+    position: [50.8768091, 4.340018]
+  },
+  contact: {
+    formspree: "https://formspree.io/xgekdvdb",
+    facebook: "https://www.facebook.com/reseauatlas",
+    email: "reseaux.atlas@gmail.com"
+  }
+}
