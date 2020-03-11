92d9f1ddbdf13b91500bdc9263a4ae32fc68829e
tperale <perale.thomas@gmail.com>
2020-03-11 21:20:28 +0100

    [add] HashRouter instead of browser router

--- src/App.js
+++ src/App.js
@@ -1,51 +1,44 @@
-import React from 'react';
-import { Footer, Navbar } from './components/';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { ThemeProvider } from 'styled-components';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import Helmet from 'react-helmet';
-import { Landing, NotFound, Post } from './routes';
+import React from "react";
+import { Footer, Navbar } from "./components/";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { ThemeProvider } from "styled-components";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Helmet from "react-helmet";
+import { Landing, NotFound, Post } from "./routes";
 
-import SETTINGS from './settings';
-
-import './assets/css/style.scss';
+import "./assets/css/style.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-library.add(fab)
-library.add(fas)
+library.add(fab);
+library.add(fas);
 
 const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./assets/css/variables.scss'); // eslint-disable-line
 
 export default () => {
   return (
-    <Router>
+    <Router basename="/">
       <ThemeProvider theme={theme}>
-        <Helmet>
-        </Helmet>
+        <Helmet></Helmet>
 
-        <Navbar/>
+        <Navbar />
 
         <Switch>
           <Route path="/" exact>
-            <Landing/>
+            <Landing />
           </Route>
           <Route path="/posts/:article">
-            <Post/>
+            <Post />
           </Route>
           <Route>
-            <NotFound/>
-          </Route >
+            <NotFound />
+          </Route>
         </Switch>
 
-        <Footer/>
+        <Footer />
       </ThemeProvider>
     </Router>
   );
-}
+};
