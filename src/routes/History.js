efcc826f566eb3936d0d805c0cd1fcccbc3955c8
tperale <perale.thomas@gmail.com>
2020-03-12 22:23:51 +0100

    [add] History

--- src/routes/History.js
+++ src/routes/History.js
@@ -44,11 +44,24 @@ export default () => {
                   "L'association des cercles facultaires et la commission culturelle officialise l'association sous la présidence de Michèle Descy et Evelyne Engels. Le Cercle se situe alors à l'époque au deuxième étage du Centre de calcul de l'ULB au Solbosch."
               },
               {
+                date: "1981",
+                title: "Premier OS ou O.S à moelle",
+                content:
+                  "Appelé 'canal 69', puis 'le clou', l'O.S à Moelle l'Organe Officiel du Cercle Informatique prend son nom en 1981. Ce journal reflète l'esprit du CI axé sur l'autodérision et le second degré."
+              },
+              {
                 date: "1984",
                 title: "Déménagement à la Plaine",
                 content:
                   "Suite au nombre croissant de personnes qui fréquentent le cercle, le centre de calcul interdit l'accès au Cercle à cause de ses activités qui ne conviennent pas à celle du Centre de calcul. Le Cercle va se voir attribuer son local actuel qui était anciennement utilisé par Radio Campus.",
                 image: require("../assets/img/history/local.jpg")
+              },
+              {
+                date: "1990",
+                title: "Premier édition de la MTSM (Miss T-Shirt Mouillé)",
+                content:
+                  "Cet évènement qui aujourd'hui n'existe heureusement plus, avait lieu pendant la semaine folklorique au second quadrimestre et consistait comme son titre l'indique à renversé des seaux d'eau fraiche sur les participant.e.s et ainsi rendre leur T-Shirt transparant.",
+                image: require("../assets/img/history/mtsm.jpg")
               },
               {
                 date: "1993",
@@ -61,6 +74,13 @@ export default () => {
                 title: "Création de l'ordre Babbage",
                 content:
                   "Avec une allusion évidente à un des pionnier de l'informatique, l'Ordre veut rassembler les anciens étudiants et étudiants d'informatique ayant oeuvré pour le cercle ou le département."
+              },
+              {
+                date: "1997",
+                title: "Premier Bal Informatique et Forum de l'emploi",
+                content:
+                  "Ce premier bal était précédé à l'époque d'un forum de l'emploi qui mettait en relation les étudiants d'informatique avec les entreprises. Cet évènement a été par la suite séparé en deux évènements distincts. Aujourd'hui le forum de l'emploi n'existe plus sous sa forme original mais a été repris par le Cercle des Sciences pour offrir une Jobfair pour l'ensemble de la facultée.",
+                image: require("../assets/img/history/forum.jpg")
               },
               {
                 date: "2001",
