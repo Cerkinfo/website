019e07d10475a4fa264bc926698a8f8855608f08
tperale <perale.thomas@gmail.com>
2020-03-12 15:58:37 +0100

    [add] Dropdown to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,10 @@ import {
   NavbarBrand,
   Navbar,
   NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
   Nav,
   NavLink
 } from "reactstrap";
@@ -30,39 +34,45 @@ export default () => {
         </button>
         <UncontrolledCollapse navbar toggler="#navbar_global">
           <Nav className="align-items-lg-center ml-lg-auto" navbar>
-            <NavItem>
-              <NavLink>
-                <NavHashLink className="font-white" smooth to="/#news">
+            <UncontrolledDropdown>
+              <DropdownToggle>Le Cercle</DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem className="font-white" href="/#news">
                   News
-                </NavHashLink>
+                </DropdownItem>
+                <DropdownItem className="font-white" href="/#cercle">
+                  {"C'est quoi"}
+                </DropdownItem>
+                <DropdownItem className="font-white" href="/#urlab">
+                  Urlab
+                </DropdownItem>
+                <DropdownItem className="font-white" href="/#dochub">
+                  Dochub
+                </DropdownItem>
+                <DropdownItem className="font-white" href="/#gates">
+                  Guilde Gates
+                </DropdownItem>
+                <DropdownItem className="font-white" href="/#babbage">
+                  Babbage
+                </DropdownItem>
+                <DropdownItem className="font-white" href="/#location">
+                  Notre Local
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="/history" className="font-white">
+                Historique
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink>
-                <NavHashLink className="font-white" smooth to="/#cercle">
-                  Le Cercle
-                </NavHashLink>
+              <NavLink href="/pv" className="font-white">
+                PV
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink>
-                <NavHashLink className="font-white" to="/history">
-                  Historique
-                </NavHashLink>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                <NavHashLink className="font-white" to="/pv">
-                  PV
-                </NavHashLink>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                <NavHashLink className="font-white" smooth to="/#contact">
-                  Contacter
-                </NavHashLink>
+              <NavLink href="/#contact" className="font-white">
+                Contacter
               </NavLink>
             </NavItem>
             <NavSocial />
