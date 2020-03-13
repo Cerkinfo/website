759c916b42dba1d2726029790cc6ad2877ae9493
tperale <perale.thomas@gmail.com>
2020-03-13 14:33:17 +0100

    [add] COdex

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,3 +4,4 @@ export { default as Pv } from "./Pv";
 export { default as NotFound } from "./404";
 export { default as Post } from "./Post";
 export { default as Statuts } from "./Statuts";
+export { default as Folklore } from "./Folklore";
