65ce05f5d57ee972b376ab18e66fa21e77202f64
Mosfet171 <arkadym@live.be>
2023-04-22 14:24:16 +0200

    Updated as planned. Missing PVs, must do recent timeline

--- src/settings.js
+++ src/settings.js
@@ -4,7 +4,7 @@
 export default {
   title: process.env.REACT_APP_WEBSITE_TITLE,
   short_title: process.env.REACT_APP_WEBSITE_SHORT_TITLE,
-  subtitle: "Sex & Choppes",
+  subtitle: "Sex & Chopes",
   description: process.env.REACT_APP_WEBSITE_DESC,
   url: process.env.REACT_APP_WEBSITE_URL,
   location: {
@@ -18,7 +18,7 @@ export default {
     town: "Ixelles",
     position: [50.8198763, 4.3990723],
     phone: "02 650 54 88",
-    bank: "BE02 0014 0193 5340",
+    bank: "BE39 9675 6578 6919",
     additional:
       "Notre local est situé sur le <i>Campus de la Plaine</i>, à deux pas de tes salles de cours et de la Jefke. Il se trouve en <i>dessous des forums</i>. En te penchant par la rambarde devant l'entrée de la cafétéria, tu peux apercevoir la porte d'entrée, plus communément appelée la <i>porte jaune</i>."
   },
